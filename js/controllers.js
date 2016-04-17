a65d8f8a1c373fab8a5c5567926d88dc4bc63ea3
krmechl <krmechl@gmail.com>
2016-04-17 16:24:49 -0400

    test

---
+++ js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('app.controllers', [])
+  
+.controller('cameraCtrl', function($scope) {
+
+})
+   
+.controller('diagnosisCtrl', function($scope) {
+
+})
+   
+.controller('cloudCtrl', function($scope) {
+
+})
+         
+.controller('cYTOLOGYPORTALCtrl', function($scope) {
+
+})
+ 
